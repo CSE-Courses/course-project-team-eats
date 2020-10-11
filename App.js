1f72e37a1bd95ed9d39ca3435fea495b5e90d266
Rakshit Dwivedi <64805932+rkd0608@users.noreply.github.com>
2020-10-12 01:44:25 +0530

    Add files via upload
    React files for website layout

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {LandingPage} from './LandingPage/LandingPage';
+import {Route, Switch} from 'react-router-dom';
+import {Search} from './Search/Search';
+
+function App() {
+  return (
+    <Switch>
+      <Route path='/search' component = {Search}/>
+      <Route path='/' component = {LandingPage} />
+    </Switch>
+    
+  );
+}
+
+export default App;

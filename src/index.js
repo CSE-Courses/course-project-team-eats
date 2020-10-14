a080f41fafa630f8bd24ff5ad4d50bb01aa0bd63
Rakshit Dwivedi <rkshtdwivedi8@gmail.com>
2020-10-14 17:37:50 +0530

    bulma issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'bulma/css/bulma.css';
+import './bulma/css/bulma.css';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 

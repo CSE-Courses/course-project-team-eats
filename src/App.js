d82cb918f76b0985f92cb9b42c79922334425e71
rickytikki <32965847+rickytikki@users.noreply.github.com>
2020-11-10 10:32:18 -0500

    Login and Signup pages User story #5
    Tasks #53 and #55

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
 import React from 'react';
-import {LandingPage} from './LandingPage/LandingPage';
-import {Route, Switch} from 'react-router-dom';
-import {Search} from './Search/Search';
-import { People } from './People/People';
-import { StarterPage } from './StarterPage/StarterPage';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './Signup.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
-  return (
-    <Switch>
-      <Route path='/search' component = {Search}/>
-      <Route exact path='/' component = {StarterPage} />
-      <Route path='/landing' component={LandingPage}/>
-      <Route path='/people' component={People}/>
-    </Switch>
-    
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
+
+function Signup() {
+  return (<Router>
+    <div className="Signup">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>Eats</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
   );
 }
 
-export default App;
+export default Signup;
